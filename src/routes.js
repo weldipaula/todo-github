72fbbdb1ced86ec465273cea9a5361af13967784
weldipaula <weldipaula@hotmail.com>
2022-05-20 13:42:27 -0300

    resolvido bug do useNavigate nas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate  } from 'react-router-dom'
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route  } from 'react-router-dom'
 
 import { Home } from './Pages/Home/index.js'
 import { Login } from './Pages/Login/index.js'
